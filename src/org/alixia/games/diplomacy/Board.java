5a3cbef559d381193ab2454ecf690f8dfe69e1de
Zeale <a@infinityminers.com>
2018-04-10 22:20:55 -0500

    Updated the board and everything that gets put on it

--- src/org/alixia/games/diplomacy/Board.java
+++ src/org/alixia/games/diplomacy/Board.java
@@ -1,24 +1,72 @@
 package org.alixia.games.diplomacy;
 
+import java.util.Objects;
+
+import org.alixia.games.diplomacy.BoardEntity.Type;
+
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.collections.ListChangeListener;
+import javafx.scene.Node;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 public final class Board extends Pane {
 
+	// Update piece sizes when added
+	{
+		getChildren().addListener(new ListChangeListener<Node>() {
+
+			@Override
+			public void onChanged(Change<? extends Node> c) {
+				while (c.next())
+					if (c.wasAdded())
+						for (Node n : c.getAddedSubList())
+							if (n instanceof ImageView && BoardEntity.isBoardEntityImageView((ImageView) n)) {
+								ImageView img = (ImageView) n;
+								img.fitWidthProperty().bind(widthProperty.divide(getBoardSize()));
+								img.fitHeightProperty().bind(heightProperty.divide(getBoardSize()));
+							} else
+								;
+					else
+						for (Node n : c.getRemoved())
+							if (n instanceof ImageView && BoardEntity.isBoardEntityImageView((ImageView) n)) {
+
+								ImageView img = (ImageView) n;
+
+								img.fitWidthProperty().unbind();
+								img.fitHeightProperty().unbind();
+
+								// Also unbind the position that it has been bound to.
+
+								n.layoutXProperty().unbind();
+								n.layoutYProperty().unbind();
+							}
+			}
+		});
+	}
+
 	private DoubleProperty widthProperty = new SimpleDoubleProperty(), heightProperty = new SimpleDoubleProperty();
 
-	private final ImageView grid = new ImageView("org/alixia/games/diplomacy/_resources/graphics/grid.png"),
-			background = new ImageView("org/alixia/games/diplomacy/_resources/graphics/bg.png"),
-			hit_tint = new ImageView("org/alixia/games/diplomacy/_resources/graphics/frame_dmg.png");
+	private final ImageView grid = new ImageView("/org/alixia/games/diplomacy/_resources/graphics/grid.png"),
+			background = new ImageView("/org/alixia/games/diplomacy/_resources/graphics/bg.png"),
+			hit_tint = new ImageView("/org/alixia/games/diplomacy/_resources/graphics/frame_dmg.png");
 
 	// Setting sizing for images
 	{
-		// TODO
+		grid.fitHeightProperty().bind(heightProperty);
+		grid.fitWidthProperty().bind(widthProperty);
+		background.fitHeightProperty().bind(heightProperty);
+		background.fitWidthProperty().bind(widthProperty);
+		hit_tint.fitHeightProperty().bind(heightProperty);
+		hit_tint.fitWidthProperty().bind(widthProperty);
 	}
 
-	// Drawing nodes to pane
+	public int getBoardSize() {
+		return entityMap.length;
+	}
+
+	// Building game board (laying out images)
 	{
 		getChildren().addAll(background, grid, hit_tint);
 		hit_tint.setOpacity(0);
@@ -47,14 +95,49 @@ public final class Board extends Pane {
 	 */
 	private Board(int size) {
 		entityMap = new BoardEntity[size][size];
+
+		// Adding default pieces ~ this can only be done after entityMap is set and,
+		// therefore, can't be done during object initialization.
+
+		put(new BoardEntity(Type.BLUE_PIECE), 0, 0);// This is NOT the first square, since board indices start @ 0.
+		put(new BoardEntity(Type.BLUE_PIECE), 0, 1);
+		put(new BoardEntity(Type.BLUE_PIECE), 1, 0);
+
 	}
 
 	public Board() {
 		this(8);
 	}
 
-	public boolean outsideBorders(int row, int col) {
-		return row > entityMap.length || row < 0 || col > entityMap.length || col < entityMap.length;
+	private boolean outsideBorders(int row, int col) {
+		return row > entityMap.length || row < 0 || col > entityMap.length || col < 0;
+	}
+
+	public BoardEntity put(BoardEntity entity, int row, int col) {
+		Objects.requireNonNull(entity);
+		if (outsideBorders(row, col))
+			throw new RuntimeException("Position outside of board borders.");
+
+		// Remove previous entity from board
+		BoardEntity previousEntity = entityMap[row][col];
+		if (previousEntity != null)
+			getChildren().remove(previousEntity.icon);
+
+		// Add new entity to board.
+		entityMap[row][col] = entity;
+		getChildren().add(entity.icon);
+
+		setPos(entity, row, col);
+
+		return previousEntity;
+
+	}
+
+	private void setPos(BoardEntity entity, int row, int col) {
+		// Every time these methods are called, the previous binds are overridden. (I
+		// think.)
+		entity.icon.layoutXProperty().bind(widthProperty.multiply(row).divide(getBoardSize()));
+		entity.icon.layoutYProperty().bind(heightProperty.multiply(col).divide(getBoardSize()));
 	}
 
 }
